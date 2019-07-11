247584ef4c1fb59e8367f5ff703110e58e1ccb6b
Raken85 <anoin1985@gmail.com>
2019-07-11 09:36:23 +0500

    issue8

--- local/templates/.default/components/bitrix/sale.order.ajax/template1/template.php
+++ local/templates/.default/components/bitrix/sale.order.ajax/template1/template.php
@@ -364,7 +364,16 @@ else
 					</div>
 					<div class="bx-soa-section-content container-fluid"></div>
 				</div>
-
+                <!--	BUYER PROPS BLOCK	-->
+                <div id="bx-soa-properties" data-visited="false" class="bx-soa-section bx-active">
+                    <div class="bx-soa-section-title-container">
+                        <h2 class="bx-soa-section-title col-sm-9">
+                            <span class="bx-soa-section-title-count"></span><?=$arParams['MESS_BUYER_BLOCK_NAME']?>
+                        </h2>
+                        <div class="col-xs-12 col-sm-3 text-right"><a href="" class="bx-soa-editstep"><?=$arParams['MESS_EDIT']?></a></div>
+                    </div>
+                    <div class="bx-soa-section-content container-fluid"></div>
+                </div>
 				<? if ($arParams['DELIVERY_TO_PAYSYSTEM'] === 'p2d'): ?>
 					<!--	PAY SYSTEMS BLOCK	-->
 					<div id="bx-soa-paysystem" data-visited="false" class="bx-soa-section bx-active">
@@ -428,16 +437,6 @@ else
 						<div class="bx-soa-section-content container-fluid"></div>
 					</div>
 				<? endif ?>
-				<!--	BUYER PROPS BLOCK	-->
-				<div id="bx-soa-properties" data-visited="false" class="bx-soa-section bx-active">
-					<div class="bx-soa-section-title-container">
-						<h2 class="bx-soa-section-title col-sm-9">
-							<span class="bx-soa-section-title-count"></span><?=$arParams['MESS_BUYER_BLOCK_NAME']?>
-						</h2>
-						<div class="col-xs-12 col-sm-3 text-right"><a href="" class="bx-soa-editstep"><?=$arParams['MESS_EDIT']?></a></div>
-					</div>
-					<div class="bx-soa-section-content container-fluid"></div>
-				</div>
 
 				<? if ($arParams['BASKET_POSITION'] === 'after'): ?>
 					<!--	BASKET ITEMS BLOCK	-->
