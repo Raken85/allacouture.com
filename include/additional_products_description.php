65c99d7f87d6c6c2b22f3fb9114a1610352cfd4c
Raken85 <v.a.anohin.work@gmail.com>
2019-07-26 11:16:11 +0500

    issue63

---
+++ include/additional_products_description.php
@@ -0,0 +1,2 @@
+<span class="ym-viewport" style="display: block; top: 0px; right: 0px; bottom: 0px; left: 0px; position: fixed; transform: translate(0px, -100%); transform-origin: 0px 0px;">Б</span>
+Здесь будут условия оплаты
