6b20e943c84f61f22c1df2cf8a3c2cd158f111fe
Raken85 <v.a.anohin.work@gmail.com>
2019-07-26 15:46:36 +0500

    issue65

--- include/menu/menu.top.php
+++ include/menu/menu.top.php
@@ -1,24 +1,24 @@
 <?global $arTheme;?>
 <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	"top",
-	array(
-		"ALLOW_MULTI_SELECT" => "Y",
-		"CHILD_MENU_TYPE" => "left",
-		"COMPONENT_TEMPLATE" => "top",
-		"COUNT_ITEM" => "6",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "4",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "3600000",
-		"MENU_CACHE_TYPE" => "N",
-		"MENU_CACHE_USE_GROUPS" => "N",
-		"CACHE_SELECTED_ITEMS" => "N",
-		"ROOT_MENU_TYPE" => "top_content_multilevel",
-		"USE_EXT" => "Y",
-		"COMPOSITE_FRAME_MODE" => "A",
-		"COMPOSITE_FRAME_TYPE" => "DYNAMIC_WITH_STUB_LOADING"
-	),
-	false
+    "bitrix:menu",
+    "top_fixed",
+    array(
+        "ALLOW_MULTI_SELECT" => "Y",
+        "CHILD_MENU_TYPE" => "left",
+        "COMPONENT_TEMPLATE" => "top_fixed",
+        "COUNT_ITEM" => "6",
+        "DELAY" => "N",
+        "MAX_LEVEL" => "3",
+        "MENU_CACHE_GET_VARS" => array(
+        ),
+        "MENU_CACHE_TIME" => "3600000",
+        "MENU_CACHE_TYPE" => "N",
+        "MENU_CACHE_USE_GROUPS" => "N",
+        "CACHE_SELECTED_ITEMS" => "N",
+        "ROOT_MENU_TYPE" => "top_content_multilevel",
+        "USE_EXT" => "Y",
+        "COMPOSITE_FRAME_MODE" => "A",
+        "COMPOSITE_FRAME_TYPE" => "DYNAMIC_WITH_STUB_LOADING"
+    ),
+    false
 );?>
