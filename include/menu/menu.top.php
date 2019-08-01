744793912ad10d8df9dd883c1b49026c693cefbc
Raken85 <v.a.anohin.work@gmail.com>
2019-08-01 14:12:46 +0500

    issue14

--- include/menu/menu.top.php
+++ include/menu/menu.top.php
@@ -1,14 +1,14 @@
 <?global $arTheme;?>
 <?$APPLICATION->IncludeComponent(
     "bitrix:menu",
-    "top_fixed",
+    "top",
     array(
         "ALLOW_MULTI_SELECT" => "Y",
         "CHILD_MENU_TYPE" => "left",
-        "COMPONENT_TEMPLATE" => "top_fixed",
+        "COMPONENT_TEMPLATE" => "top",
         "COUNT_ITEM" => "6",
         "DELAY" => "N",
-        "MAX_LEVEL" => "3",
+        "MAX_LEVEL" => "4",
         "MENU_CACHE_GET_VARS" => array(
         ),
         "MENU_CACHE_TIME" => "3600000",
