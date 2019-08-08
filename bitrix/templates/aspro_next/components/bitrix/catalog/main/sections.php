ac01b3b867b0300d67e92df0b0e41809ddca5bba
Raken85 <v.a.anohin.work@gmail.com>
2019-08-08 16:45:17 +0500

    issue19

--- bitrix/templates/aspro_next/components/bitrix/catalog/main/sections.php
+++ bitrix/templates/aspro_next/components/bitrix/catalog/main/sections.php
@@ -1,3 +1,11 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?$this->setFrameMode(true);?>
+<style>
+    .sections_wrapper{
+        margin-top: 40px;
+    }
+    .wraps > .wrapper_inner {
+        padding-bottom: 10px;
+    }
+</style>
 <?@include_once('page_blocks/'.$arParams["SECTIONS_TYPE_VIEW"].'.php');?>	
--- local/templates/.default/components/bitrix/catalog.section.list/sections_list_custom/template.php
+++ local/templates/.default/components/bitrix/catalog.section.list/sections_list_custom/template.php
@@ -1,51 +1,38 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <? $this->setFrameMode( true ); ?>
-<?if($arResult["SECTIONS"]){?>
-<div class="catalog_section_list row items flexbox catalog_section_list_ikonki">
-	<?foreach( $arResult["SECTIONS"] as $arItems ){
-		$this->AddEditAction($arItems['ID'], $arItems['EDIT_LINK'], CIBlock::GetArrayByID($arItems["IBLOCK_ID"], "SECTION_EDIT"));
-		$this->AddDeleteAction($arItems['ID'], $arItems['DELETE_LINK'], CIBlock::GetArrayByID($arItems["IBLOCK_ID"], "SECTION_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_SECTION_DELETE_CONFIRM')));
-	?>
-		<div class="item_block col-md-3 col-sm-6">
-			<div class="section_item item" id="<?=$this->GetEditAreaId($arItems['ID']);?>">
-				<div class="section_item_inner catalog_razdeli_ikonki">
-						<?if ($arParams["SHOW_SECTION_LIST_PICTURES"]=="Y"):?>
-							<?$collspan = 2;?>
-							<div class="image">
-								<?if($arItems["PICTURE"]["SRC"]):?>
-									<?$img = CFile::ResizeImageGet($arItems["PICTURE"]["ID"], array( "width" => 220, "height" => 220 ), BX_RESIZE_IMAGE_EXACT, true );?>
-									<a href="<?=$arItems["SECTION_PAGE_URL"]?>" class="thumb"><img src="<?=$arItems["PICTURE"]["SRC"]?>" alt="<?=($arItems["PICTURE"]["ALT"] ? $arItems["PICTURE"]["ALT"] : $arItems["NAME"])?>" title="<?=($arItems["PICTURE"]["TITLE"] ? $arItems["PICTURE"]["TITLE"] : $arItems["NAME"])?>" /></a>
-								<?elseif($arItems["~PICTURE"]):?>
-									<?$img = CFile::ResizeImageGet($arItems["~PICTURE"], array( "width" => 220, "height" => 220 ), BX_RESIZE_IMAGE_EXACT, true );?>
-									<a href="<?=$arItems["SECTION_PAGE_URL"]?>" class="thumb"><img src="<?=$arItems["PICTURE"]["SRC"]?>" alt="<?=($arItems["PICTURE"]["ALT"] ? $arItems["PICTURE"]["ALT"] : $arItems["NAME"])?>" title="<?=($arItems["PICTURE"]["TITLE"] ? $arItems["PICTURE"]["TITLE"] : $arItems["NAME"])?>" /></a>
-								<?else:?>
-									<a href="<?=$arItems["SECTION_PAGE_URL"]?>" class="thumb"><img src="<?=SITE_TEMPLATE_PATH?>/images/catalog_category_noimage.png" alt="<?=$arItems["NAME"]?>" title="<?=$arItems["NAME"]?>" /></a>
-								<?endif;?>
-							</div>
-						<?endif;?>
-						<div class="section_info">
-							<ul>
-								<li class="name">
-									<a href="<?=$arItems["SECTION_PAGE_URL"]?>" class="dark_link"><span><?=$arItems["NAME"]?></span></a>
-								</li>
-								<?if($arItems["SECTIONS"]){
-									foreach( $arItems["SECTIONS"] as $arItem ){?>
-										<li class="sect"><a href="<?=$arItem["SECTION_PAGE_URL"]?>" class="dark_link"><?=$arItem["NAME"]?><? echo $arItem["ELEMENT_CNT"]?'&nbsp;<span>'.$arItem["ELEMENT_CNT"].'</span>':'';?></a></li>
-									<?}
-								}?>
-							</ul>
-						</div>
-					<?if($arParams["SECTIONS_LIST_PREVIEW_DESCRIPTION"]!="N"):?>
-						<?$arSection = $section=CNextCache::CIBlockSection_GetList(array('CACHE' => array("MULTI" =>"N", "TAG" => CNextCache::GetIBlockCacheTag($arParams["IBLOCK_ID"]))), array('GLOBAL_ACTIVE' => 'Y', "ID" => $arItems["ID"], "IBLOCK_ID" => $arParams["IBLOCK_ID"]), false, array("ID", $arParams["SECTIONS_LIST_PREVIEW_PROPERTY"]));?>
-						<?/*if ($arSection[$arParams["SECTIONS_LIST_PREVIEW_PROPERTY"]]):?>
-							<tr><td class="desc" <?=($collspan? 'colspan="'.$collspan.'"':"");?>><span class="desc_wrapp"><?=$arSection[$arParams["SECTIONS_LIST_PREVIEW_PROPERTY"]]?></span></td></tr>
-						<?else:?>
-							<tr><td class="desc" <?=($collspan? 'colspan="'.$collspan.'"':"");?>><span class="desc_wrapp"><?=$arItems["DESCRIPTION"]?></span></td></tr>
-						<?endif;*/?>
-					<?endif;?>
-				</div>
-			</div>
-		</div>
-	<?}?>
-</div>
-<?}?>
+<?if($arResult['SECTIONS']):?>
+    <div class="sections_wrapper">
+        <?if($arParams["TITLE_BLOCK"] || $arParams["TITLE_BLOCK_ALL"]):?>
+            <div class="top_block">
+                <h3 class="title_block"><?=$arParams["TITLE_BLOCK"];?></h3>
+                <a href="<?=SITE_DIR.$arParams["ALL_URL"];?>"><?=$arParams["TITLE_BLOCK_ALL"] ;?></a>
+            </div>
+        <?endif;?>
+        <div class="list items">
+            <div class="row margin0 flexbox">
+                <?foreach($arResult['SECTIONS'] as $arSection):
+                    $this->AddEditAction($arSection['ID'], $arSection['EDIT_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "ELEMENT_EDIT"));
+                    $this->AddDeleteAction($arSection['ID'], $arSection['DELETE_LINK'], CIBlock::GetArrayByID($arSection["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));?>
+                    <div class="col-m-20 col-md-3 col-sm-4 col-xs-6">
+                        <div class="item" id="<?=$this->GetEditAreaId($arSection['ID']);?>">
+                            <div class="img shine">
+                                <?if($arSection["PICTURE"]["SRC"]):?>
+                                    <?$img = CFile::ResizeImageGet($arSection["PICTURE"]["ID"], array( "width" => 120, "height" => 120 ), BX_RESIZE_IMAGE_EXACT, true );?>
+                                    <a href="<?=$arSection["SECTION_PAGE_URL"]?>" class="thumb"><img src="<?=$arSection["PICTURE"]["SRC"]?>" alt="<?=($arSection["PICTURE"]["ALT"] ? $arSection["PICTURE"]["ALT"] : $arSection["NAME"])?>" title="<?=($arSection["PICTURE"]["TITLE"] ? $arSection["PICTURE"]["TITLE"] : $arSection["NAME"])?>" /></a>
+                                <?elseif($arSection["~PICTURE"]):?>
+                                    <?$img = CFile::ResizeImageGet($arSection["~PICTURE"], array( "width" => 120, "height" => 120 ), BX_RESIZE_IMAGE_EXACT, true );?>
+                                    <a href="<?=$arSection["SECTION_PAGE_URL"]?>" class="thumb"><img src="<?=$arSection["PICTURE"]["SRC"]?>" alt="<?=($arSection["PICTURE"]["ALT"] ? $arSection["PICTURE"]["ALT"] : $arSection["NAME"])?>" title="<?=($arSection["PICTURE"]["TITLE"] ? $arSection["PICTURE"]["TITLE"] : $arSection["NAME"])?>" /></a>
+                                <?else:?>
+                                    <a href="<?=$arSection["SECTION_PAGE_URL"]?>" class="thumb"><img src="<?=SITE_TEMPLATE_PATH?>/images/no_photo_medium.png" alt="<?=$arSection["NAME"]?>" title="<?=$arSection["NAME"]?>" height="90" /></a>
+                                <?endif;?>
+                            </div>
+                            <div class="name">
+                                <a href="<?=$arSection['SECTION_PAGE_URL'];?>" class="dark_link"><?=$arSection['NAME'];?></a>
+                            </div>
+                        </div>
+                    </div>
+                <?endforeach;?>
+            </div>
+        </div>
+    </div>
+<?endif;?>
